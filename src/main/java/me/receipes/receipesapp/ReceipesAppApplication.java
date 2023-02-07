1fae9edbcfc015fae19412b6ef9c6a64ce7bea7d
MaysVerrecas <99118212+MaysVerrecas@users.noreply.github.com>
2023-02-08 01:13:28 +0300

    firstcommit

---
+++ src/main/java/me/receipes/receipesapp/ReceipesAppApplication.java
@@ -0,0 +1,13 @@
+package me.receipes.receipesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceipesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/receipes/receipesapp/ReceipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.receipes.receipesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
